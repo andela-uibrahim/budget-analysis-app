5b971efc681560697594712e9a7e9be8f1a9a981
usman ibrahim <usman.ibrahim@outlook.com>
2017-01-19 19:30:09 -0500

    Bugs fixed

--- js/login.js
+++ js/login.js
@@ -1,50 +1,13 @@
-    
-    
-    //add login event
-    
-login.addEventListener('click', e => {
-    // get email and pass
-    const email = $('#email').val();
-    const pass = $('#password').val();
-    const login = $('#login').val();
-    const auth = firebase.auth();
-    
-    //sign in
-    const promise= auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-})
-
-// add signUp event
-    signUp.addEventListener('click', e => {
-    // get email and pass
-    const email = $('#email').val();
-    const pass = $('#password').val();
-    const signUp = $('#signUp').val();
-    const auth = firebase.auth();
-    
-    //sign in
-    const promise= auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
+$(document).ready(function(){
+    $("#login").click(function(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        var user;
+        firebase.auth().signInWithPopup(provider)
+        .then(function(result){
+            console.log(result.user)
+    }).catch(function(err) {
+        console.log(err)
     })
-    
-//log out
-logout.addEventListener('click', e => {
-    // get email and pass
-   firebase.auth().signOut();
-})
-
-// add reatime listener
-firebase.auth().onAuthStateChanged (firebaseUser => {    
-    if (firebaseUser){
-        console.log(firebaseUser);
-        logout.classList.remove('hide');
-        
-    }else{
-        console.log('not logged in');
-        logout.classList.add('hide');
-    }
-
-
+    })
+   
 });
-
-}());
